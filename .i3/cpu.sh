e6120ad61ce186ebb92624f61780f10d2034cbfc
Alex Ling <hkalexling@gmail.com>
2023-05-07 13:50:00 -0700

    Fix cpu governor script

--- .i3/cpu.sh
+++ .i3/cpu.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+set -e
+
 governors="$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors)"
 choice="$1"
 
@@ -10,6 +12,15 @@ fi
 
 [[ -z "$(echo "$governors" | grep -w "$choice")" ]] && echo "unknown policy $choice" && exit 1
 
+# Thanks ChatGPT
+temp_script=$(mktemp)
+cat > "$temp_script" <<- EOM
+#!/bin/sh
+choice="$choice"
 for num in 0 1 2 3 4 5 6 7; do
-	echo "$choice" > "/sys/devices/system/cpu/cpu$num/cpufreq/scaling_governor"
+  echo "\$choice" > /sys/devices/system/cpu/cpu\${num}/cpufreq/scaling_governor
 done
+EOM
+chmod +x "$temp_script"
+pkexec "$temp_script"
+rm -f "$temp_script"
